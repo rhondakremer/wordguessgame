bdad0da56defd0ea390552b3af144daf5a45dcdd
Rhonda Kremer <rhondakremer@gmail.com>
2019-06-28 13:48:40 -0400

    correct letters are recognized and display in array

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,35 +1,83 @@
-// create an array of words
-var wordBank = ["apple", "banana", "grapes", "starfruit", "pineapple", "jackfruit", "avocado"];
+$(document).ready(function() {
 
-//set number of guesses
-var guesses = 26;
 
-//pick a word from array
-var rand = wordBank[(Math.random() * wordBank.length) | 0]
-console.log(rand);
+var wordBank = ["apple", "banana", "grapes", "starfruit", "pineapple", "jackfruit", "avocado"];//array of words to guess
+var guessesLeft = 10;//number of guesses left
+//inputArray = user's guess
+var inputArray = [];//random displayed as _
+var wins = 0;//# of wins
+var losses = 0;//# of losses
+var guessedLetters = [];
+var randomWord = wordBank[Math.floor(Math.random() * wordBank.length)];//pick random word from workBank array
+var wordAsArray = randomWord.split('');//splits randomWord
 
-//display word as underscores
-var rand;
-var wordLength = rand.length;
-var underscores = "";
-for(i=0; i<wordLength; i++) {
-    underscores = underscores + "_ "
+console.log(wordAsArray);
+console.log(randomWord);
+//display word on screen as underscores
+for (let i = 0; i < randomWord.length; i++) {
+    inputArray[i] = "_ ";
+    $("#currentWord").append("<span>"+inputArray[i]+"</span>")
 }
-document.write(underscores);
+
+//var updateGuessesLeft = function () {
+ //    document.querySelector('#guessesLeft').innerHTML = guessesLeft;
+//}
+
+//var updateGuessesSoFar = function () {
+    document.querySelector('#guessesSoFar').innerHTML = guessedLetters.join(', ');
+//}
+
+//var updateLetterToGuess;
+
+
 
 //record character inputted from user
-var keyPress;
+
 document.addEventListener('keydown', function(event) {
     console.log(event.key);
+    //guessesLeft--;
+    //console.log(guessesLeft);
+    var letter = event.key.toLowerCase();
+    //updateGuessesLeft();
+    //updateGuessesSoFar();
+    //IF it matches, reveal letter
+    for (let i = 0; i < randomWord.length; i++) {
+        if (letter == wordAsArray[i]) {
+            inputArray[i] = letter;
+            document.getElementById("currentWord").innerHTML = inputArray.join("");
+        }
+        
+    }
+    /*
+        //wins++;
+       // document.querySelector('#wins').innerHTML = wins;
+        document.querySelector('#currentWord').innerHTML = letter;
+        //where this is TRUE, print out the leter. The letter is the input.So it's where the input = letter in random word
+        //reset();
+    };*/
+
+
+
     }
 )
 
-//check if character inputted matches any from word
+//check if character inputted matches any from word for (if char equals)
 
 
 
-//IF it matches, reveal letter
+
+
+var reset = function () {}
+
+//var win  function (){};
+//var losses 
+
 //ELSE add letter to guessed column and subtract one from guesses remaining
+
 //do not accept character that has already been guessed
+
 //when all letters are guessed, add 1 to wins total
-//when 1 is added to wins, generate new word and reset letters guessed
+
+//when 1 is added to wins, generate new word and reset letters guessed */
+
+});
---
+++ assets/javascript/tutoringjs.js
@@ -0,0 +1,141 @@
+$(document).ready(function() {
+
+    // create an array of words
+    var wordBank = ["apple", "banana", "grapes", "starfruit", "pineapple", "jackfruit", "avocado"];
+    
+    //set number of guesses
+    var guessesLeft = 10;
+    //inputArray = user's guess
+    var inputArray = [];
+    var wins = 0;
+    var losses = 0;
+    var guessedLetters = [];
+    
+    //pick a word from array
+    var rand = wordBank[Math.floor(Math.random() * wordBank.length)];
+    console.log(rand);
+    
+    //record character inputted from user (why doesn't this work after next step?)
+    
+    //display word on screen as underscores
+    for (let i = 0; i < rand.length; i++) {
+        inputArray[i] = "_ ";
+        $("#currentWord").append("<span>"+inputArray[i]+"</span>")
+    }
+    
+    var updateGuessesLeft = function () {
+         document.querySelector('#currentWord').innerHTML = guessesLeft;
+    }
+    
+    var updateGuessesSoFar = function () {
+        //user guesses displayed as letters separated by a comma
+        document.querySelector('#guesses-so-far').innerHTML = guessedLetters.join(', ');
+    }
+    
+    var updateLetterToGuess;
+    
+    document.addEventListener('keydown', function(event) {
+        console.log(event.key);
+        guessesLeft--;
+        console.log(guessesLeft);
+        var letter = event.key.toLowerCase();
+        inputArray.push(letter);
+        updateGuessesLeft();
+        updateGuessesSoFar();
+        //IF it matches, reveal letter
+        var match = rand.includes(letter);
+    
+    
+        console.log(match);
+        //checking if there's a match between user Input and randomly selected word
+        //.includes() will check if it 'includes in a function above
+        if (match == true ) {
+            wins++;
+            document.querySelector('#wins').innerHTML = wins;
+            document.query('#___').innerHTML
+            //where this is TRUE, print out the leter. The letter is the input.So it's where the input = letter in random word
+            reset();
+        };
+    
+    
+    
+        }
+    )
+    
+    //check if character inputted matches any from word for (if char equals)
+    
+    
+    
+    
+    
+    var reset = function () {}
+    
+    //var win  function (){};
+    //var losses 
+    
+    //ELSE add letter to guessed column and subtract one from guesses remaining
+    
+    //do not accept character that has already been guessed
+    
+    //when all letters are guessed, add 1 to wins total
+    
+    //when 1 is added to wins, generate new word and reset letters guessed */
+    
+    });
+
+
+
+    //var updateGuessesLeft = function () {
+ //    document.querySelector('#guessesLeft').innerHTML = guessesLeft;
+//}
+
+//var updateGuessesSoFar = function () {
+    document.querySelector('#guessesSoFar').innerHTML = guessedLetters.join(', ');
+//}
+
+//var updateLetterToGuess;
+
+
+
+//record character inputted from user
+
+
+console.log(inputArray);
+    console.log(match);
+    //checking if there's a match between user Input and randomly selected word
+    //.includes() will check if it 'includes in a function above
+    if (match == true ) {
+        for (let index = 0; index < rand.length; index++) {
+            inputArray[index] = letter;
+            
+        }
+        wins++;
+        document.querySelector('#wins').innerHTML = wins;
+        //document.query('#___').innerHTML;
+        //where this is TRUE, print out the leter. The letter is the input.So it's where the input = letter in random word
+        reset();
+    };
+
+
+
+    }
+)
+
+//check if character inputted matches any from word for (if char equals)
+
+
+
+
+
+var reset = function () {}
+
+//var win  function (){};
+//var losses 
+
+//ELSE add letter to guessed column and subtract one from guesses remaining
+
+//do not accept character that has already been guessed
+
+//when all letters are guessed, add 1 to wins total
+
+//when 1 is added to wins, generate new word and reset letters guessed */
